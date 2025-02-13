4c56e4d91bce24d949a8495e21da883389e9e3fb
Ghulam Mustafa Memon <ghulammemon875@gmail.com>
2025-02-13 14:45:43 +0500

    create a frontend project and create a UI and Code review Button

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <main>
+        <div className="left">
+          <div className="code"> </div>
+          <div className="review">Code Review</div>
+        </div>
+        <div className="right"></div>
+      </main>
+    </>
+  );
+}
+
+export default App;
